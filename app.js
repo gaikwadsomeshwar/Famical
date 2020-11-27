c2a139118395e8254e0a317e57f33bb258aacc69
DARkNIGHT <gaikwadsomeshwar6@gmail.com>
2020-11-27 23:37:37 +0530

    Connect Local Database

--- app.js
+++ app.js
@@ -2,38 +2,16 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-
-app.use(bodyParser.urlencoded({extended: true}));
-
-app.set('view engine', 'ejs');
-
-// var items = ["Buy Food", "Cook Food", "Eat the Food"];
-//
-// app.get("/", function(req, res) {
-//
-//   var today = new Date();
-//
-//   var options = {
-//     weekday: "long",
-//     day: "numeric",
-//     month: "long"
-//   };
-//
-//   var day = today.toLocaleDateString("en-US", options);
-//
-//
-//   res.render("list", {
-//     kindOfDay: day,
-//     listItems: items
-//   });
-// })
-//
-// app.post("/", function(req, res) {
-//
-//   items.push(req.body.newItem);
-//   res.redirect("/");
-// })
-
 app.listen(3000, function() {
   console.log("Server Started at Port 3000");
 })
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
+app.set('view engine', 'ejs');
+
+var usersRouter = require('./routes/users');
+app.use('/users',usersRouter);
