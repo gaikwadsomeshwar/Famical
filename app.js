0cca26d44558305f130ac0db3c8384a4477a53ab
DARkNIGHT <gaikwadsomeshwar6@gmail.com>
2020-11-28 08:37:55 +0530

    Add Login and Reg Routes

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const login = require('./routes/loginroutes');
 
 const app = express();
-app.listen(3000, function() {
-  console.log("Server Started at Port 3000");
-})
+var router = express.Router();
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(express.static("public"));
+
+// To serve cross-domain requests
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.set('view engine', 'ejs');
 
-var usersRouter = require('./routes/users');
-app.use('/users',usersRouter);
+//routes to handle user registration
+router.post('/register',login.register);
+router.post('/login',login.login)
+app.use('/api', router);
+
+app.listen(3000, function() {
+  console.log("Server Started at Port 3000");
+})
